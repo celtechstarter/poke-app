a8308caf511e2c4dfc012fe8f6badc08776903cf
WillyEllwart <willy.ellwart@edu.techstarter.de>
2024-12-02 17:07:49 +0100

    radix-ui/themes und Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Theme } from '@radix-ui/themes';
 import Home from './pages/Home';
 import About from './pages/About';
 import ScanPage from './pages/Scan';
+import LoginPage from './pages/LoginPage'; 
+
 
 function App() {
   return (
+    <Theme appearance="dark" accentColor="crimson" grayColor="sand" radius="medium" scaling="95%"> 
     <Router>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/scan" element={<ScanPage />} />
       </Routes>
     </Router>
+    </Theme>
   );
 }
 
--- frontend/src/pages/Scan.jsx
+++ frontend/src/pages/Scan.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const ScanPage = () => {
   return (
     <div>
