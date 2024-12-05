e0751880a245d846f42f1d4d2eb8b0e02ed8da31
celtechstarter <marcel.welk87@gmail.com>
2024-12-05 10:39:02 +0100

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import { Theme } from '@radix-ui/themes';
 import Home from './pages/Home';
 import About from './pages/About';
 import ScanPage from './pages/Scan';
-<<<<<<< HEAD
 import LoginPage from './pages/LoginPage'; 
 import MyCards from './pages/MyCards';
 import Navbar from './components/Navbar';
@@ -23,21 +22,6 @@ function App() {
         <Route path="/my-cards" element={<MyCards />} />
       </Routes>
     </Router>
-=======
-import LoginPage from './pages/LoginPage';
-
-function App() {
-  return (
-    <Theme appearance="dark" accentColor="crimson" grayColor="sand" radius="medium" scaling="95%">
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/scan" element={<ScanPage />} />
-        </Routes>
-      </Router>
->>>>>>> 4b28d0a6ff7ae2371794f93609636e51d604623b
     </Theme>
   );
 }
