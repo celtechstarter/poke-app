899cebc3c71e0aa999c011ed9c4eafb3e0932af4
WillyEllwart <willy.ellwart@edu.techstarter.de>
2024-12-06 09:24:15 +0100

    Backgroung Logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,17 @@ import { Theme } from '@radix-ui/themes';
 import Home from './pages/Home';
 import About from './pages/About';
 import ScanPage from './pages/Scan';
-<<<<<<< HEAD
 import LoginPage from './pages/LoginPage'; 
 import MyCards from './pages/MyCards';
 import Navbar from './components/Navbar';
+import './App.css';
 
 
 function App() {
   return (
-    <Theme appearance="dark" accentColor="crimson" grayColor="sand" radius="medium" scaling="95%"> 
-    <Router>
+  
+    <Theme appearance="light" accentColor="crimson" grayColor="sand" radius="medium" scaling="95%"> 
+        <Router>
       <Navbar />
       <Routes>
         <Route path="/login" element={<LoginPage />} />
@@ -23,22 +24,8 @@ function App() {
         <Route path="/my-cards" element={<MyCards />} />
       </Routes>
     </Router>
-=======
-import LoginPage from './pages/LoginPage';
-
-function App() {
-  return (
-    <Theme appearance="dark" accentColor="crimson" grayColor="sand" radius="medium" scaling="95%">
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/scan" element={<ScanPage />} />
-        </Routes>
-      </Router>
->>>>>>> 4b28d0a6ff7ae2371794f93609636e51d604623b
-    </Theme>
+      </Theme>
+    
   );
 }
 
