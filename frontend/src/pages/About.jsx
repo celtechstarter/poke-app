0183ffd5949afea63719ad9a8aeb57765b40a76e
Christian Sawatzky <christian.sawatzky@edu.techstarter.de>
2024-12-17 23:33:39 +0100

    About Us bearbeitet - Leider hovern nicht geklappt, irgendwas stimmt nicht

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,10 +1,89 @@
 import React from 'react';
+import { Flex, Text, Button, Box, Card, Avatar } from "@radix-ui/themes"; // Sicherstellen, dass alle benötigten Komponenten importiert sind
 
 const About = () => {
   return (
     <div>
       <h1>Über uns</h1>
       <p>Informationen über die Pokémon Karten-Scanner App.</p>
+
+      <Box maxWidth="240px" style={{ marginBottom: '20px' }}
+      >
+        <Card>
+          <Flex gap="3" align="center">
+            <Avatar
+              size="3"
+              src="/images/christian.png" // Korrigierte Pfadangabe für das Bild
+              radius="full"
+              fallback="T"
+            />
+            <Box>
+              <Text as="div" size="2" weight="bold">
+                Christian Sawatzky
+              </Text>
+              <Text as="div" size="2" color="gray">
+                DevOps Engineering
+              </Text>
+              <a 
+                href="https://www.linkedin.com/in/christian-sawatzky-78b985341/" 
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+            </Box>
+          </Flex>
+        </Card>
+      </Box>
+      <Box maxWidth="240px" style={{ marginBottom: '20px'  }}
+      >
+        <Card>
+          <Flex gap="3" align="center">
+            <Avatar
+              size="3"
+              src="/images/marcel.png" // Korrigierte Pfadangabe für das Bild
+              radius="full"
+              fallback="T"
+            />
+            <Box>
+              <Text as="div" size="2" weight="bold">
+                Marcel Welk
+              </Text>
+              <Text as="div" size="2" color="gray">
+                DevOps Engineering
+              </Text>
+            </Box>
+          </Flex>
+        </Card>
+      </Box>
+      <Box maxWidth="240px">
+        <Card>
+          <Flex gap="3" align="center">
+            <Avatar
+              size="3"
+              src="/images/willy.png" // Korrigierte Pfadangabe für das Bild
+              radius="full"
+              fallback="T"
+            />
+            <Box>
+              <Text as="div" size="2" weight="bold">
+                Willy Ellwart
+              </Text>
+              <Text as="div" size="2" color="gray">
+                DevOps Engineering
+              </Text>
+              <a 
+                href="https://www.linkedin.com/in/willy-ellwart-b1977a2b8/"
+
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+            </Box>
+          </Flex>
+        </Card>
+      </Box>
     </div>
   );
 };
